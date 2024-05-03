df0155a5908b41530ca9e244e2a88dbf56f6aeaf
caue <cauepaivalira@outlook.com>
2024-05-03 18:30:48 -0300

    mudancas no html parser, e na classe de extração

--- scrapping_caracte_socioeco/html_parser.py
+++ scrapping_caracte_socioeco/html_parser.py
@@ -3,25 +3,24 @@ import re
 class MyHTMLParser():
     
    limiter_tag: str #tag externa que limita o bloco de HTML, ex: <li> até </li>
-   limiter_regex: str 
-   limiter_start_regex: str
    limiter_end_regex: str
 
    def __init__(self, limiter_tag:str)->None:
         self.limiter_tag = limiter_tag
-        self.limiter_start_regex = rf"<{limiter_tag}>"
         self.limiter_end_regex = rf"</{limiter_tag}>"
 
    def get_limited_html_block(self,raw_html:str)->str:
-         start_match = re.search(self.limiter_start_regex, raw_html)
-         if start_match:
-            start_delimiter_index = start_match.start()
-            end_delimiter_match = re.search(self.limiter_end_regex, raw_html[start_delimiter_index:])
-            if end_delimiter_match:
-                end_delimiter_index = end_delimiter_match.end() + start_delimiter_index
-                return raw_html[start_delimiter_index:end_delimiter_index]
+         """
+         Dado a tag HTML delimitadora configurada na instanciação da classe, acha a primeira tag de final </tag> desse tipo e retorna o html do começo 
+         Até essa tag. Esse método apenas funciona se o HTMl não estiver "nested" com vários níveis da mesma tag   
+         """  
          
-         raise ValueError("Não foi possível processar esse bloco de html")
+         end_delimiter_match = re.search(self.limiter_end_regex, raw_html) #acha a primeia occorencia do delimitador final da tag
+         if end_delimiter_match:
+            end_delimiter_index = end_delimiter_match.end()
+            return raw_html[:end_delimiter_index]
+         else:  #caso nenhum delimitador for achado
+            raise ValueError("Não foi possível processar esse bloco de html")
    
    def get_all_links(self,parsed_html:str)->list[str]:
       href_pattern = r'href'
@@ -46,8 +45,7 @@ class MyHTMLParser():
 
 if __name__ == "__main__":
    html = """
-</li>
-<li>Base 2010-2021 - <strong>(<a href="https://ftp.ibge.gov.br/Pib_Municipios/2021/base/base_de_dados_2010_2021_xlsx.zip">xlsx </a>| <a href="https://ftp.ibge.gov.br/Pib_Municipios/2021/base/base_de_dados_2010_2021_ods.zip">ods </a>| <a href="https://ftp.ibge.gov.br/Pib_Municipios/2021/base/base_de_dados_2010_2021_txt.zip">txt</a>)</strong></li>
+Base 2010-2021 - <strong>(<a href="https://ftp.ibge.gov.br/Pib_Municipios/2021/base/base_de_dados_2010_2021_xlsx.zip">xlsx </a>| <a href="https://ftp.ibge.gov.br/Pib_Municipios/2021/base/base_de_dados_2010_2021_ods.zip">ods </a>| <a href="https://ftp.ibge.gov.br/Pib_Municipios/2021/base/base_de_dados_2010_2021_txt.zip">txt</a>)</strong></li>
 </ul>
 <p></p>
 <h4>Mapa interativo</h4>
--- scrapping_caracte_socioeco/pib_cidades.py
+++ scrapping_caracte_socioeco/pib_cidades.py
@@ -11,11 +11,10 @@ class BaseDataType(Enum):
 
 class IbgeBasesScrapper():
 
-   REGEX_BASES_DADOS_IBGE: str = r"Base \d{4}-\d{4}"
-   OFFSET_STRING_BASES: int = 10 #quando achamos a string que representa a tag html com o link das bases, precisamos voltar 10 chars para pegar a tag que tem essa string
-   EXTRACTED_FILES_DIR:str = "tempfiles"
+   REGEX_BASES_DADOS_IBGE: str = r"Base \d{4}-\d{4}" #regex para achar a string que identifica onde está os arquivos da base de dados
+   EXTRACTED_FILES_DIR:str = "tempfiles" #diretório temporário para guardar os arquivos .zip e de dados extraidos
 
-   def extract_database(self, url: str, html_tag_identifier:str ,data_type: BaseDataType)->pd.DataFrame:
+   def extract_database(self, url: str, html_tag_identifier:str, data_type: BaseDataType)->pd.DataFrame:
       """
       Extrai um arquivo da base de dados do IBGE dado um URL para uma página do IBGE, um identificador da tag HTML que o link do arquivo está e o tipo de dado do arquivo
 
@@ -29,42 +28,50 @@ class IbgeBasesScrapper():
       """
       
       driver = selenium.webdriver.Chrome()
-      driver.get(url)
+      driver.get(url) #acessa a página do url
       page_source:str = driver.page_source #pega o HTML da página
       driver.quit() #fecha o webdriver 
 
-      databases_match = re.finditer(self.REGEX_BASES_DADOS_IBGE, page_source) #matchs no HTML com a string que identifica as bases de dados do ibge
+      databases_match = re.finditer(self.REGEX_BASES_DADOS_IBGE, page_source) #match no HTML com a string que identifica as bases de dados do ibge
    
       most_recent_data = max(databases_match,key= lambda x : x.group()) #acha a data mais recente entre as matches de banco de dados, isso por que 2010 > 2000 na comparação
 
       html_parser = MyHTMLParser(html_tag_identifier) #instancia um objeto de parser de html
-      substr:str = page_source[ (most_recent_data.start()- self.OFFSET_STRING_BASES) : ]  #substr do html que começa um pouco antes (offset negativo) do match com a str que identifica
-      #o banco de dados do ibge, isso é feito para pegar a tag HTML que o link dessa base está
+      substr:str = page_source[most_recent_data.start(): ] #corta o html da página a partir da str que representa o banco de dados do IBGE
       
-      lista_links:list[str] = html_parser.get_all_links( html_parser.get_limited_html_block(substr) ) #pega todos os links do html extraido
+      parsed_html:str = html_parser.get_limited_html_block(substr) #extrai apenas o bloco de html com a tag que tem os links dos arquivos do banco
+      lista_links:list[str] = html_parser.get_all_links( parsed_html) #pega todos os links do html extraido
       final_link:str = ""
 
       for link in lista_links: #acha o link certo para o tipo de dado passado como argumento
-         if data_type.value in link:
+         if data_type.value in link: #link pra um arquivo que tem o tipo de dado correto
                final_link = link
+               break
          
-      if not final_link:
+      if not final_link: #nenhum link do tipo de dado especificado foi extraido
          raise RuntimeError(f"não foi possível achar o link da base de dados com o tipo {data_type.value}")
-         
+      
       return self.__dataframe_from_link(final_link,data_type) #retorna o dataframe do link extraido
    
-   def __download_extract_zipfile(self,url:str)->str:
-      """Retorna o caminho para o arquivo de dados que foi baixado e extraido"""
+   def __download_and_extract_zipfile(self,url:str)->str:
+      """
+      Dado um URL para baixar um arquivo zip das bases do IBGE, baixa esse arquivo zip e extrai seu conteúdo,
+      retornando o caminho para o arquivo de dados que extraido
+
+      Args:
+         url (str) : url para baixar o arquivo .zip das bases do IBGE
+      
+      """
 
       #caso o diretório para guardar os arquivos extraidos não exista, vamos criar ele
       if not os.path.exists(self.EXTRACTED_FILES_DIR):
          os.makedirs(self.EXTRACTED_FILES_DIR)
 
       #baixando o arquivo zip
-      response = requests.get(url)
-      if response.status_code == 200:
+      response = requests.get(url) #request get para o link do arquivo zip 
+      if response.status_code == 200: #request com sucesso
          with open(os.path.join(self.EXTRACTED_FILES_DIR, "zip_file.zip"), "wb") as f:
-            f.write(response.content)
+            f.write(response.content) #escreve o arquivo zip no diretório de dados temporários
       else:
          raise RuntimeError("Falhou em baixar o arquivo .zip, status code da resposta:", response.status_code)
 
@@ -72,25 +79,31 @@ class IbgeBasesScrapper():
       with zipfile.ZipFile(os.path.join(self.EXTRACTED_FILES_DIR, "zip_file.zip"), "r") as zip_ref:
             zip_ref.extractall(self.EXTRACTED_FILES_DIR)
 
+      #no diretório de arquivos baixados e extraidos, vamos ter o .zip e o arquivo de dados extraido
       extracted_files:list[str] = os.listdir(self.EXTRACTED_FILES_DIR)
       
-      data_file:str = ""
+      data_file_name:str = ""
       for file in extracted_files:
-         if ".zip" not in file:
-            data_file = file
+         if ".zip" not in file: #acha o arquivo de dados, que é o único sem ser .zip
+            data_file_name = file
             break
 
-      if not extracted_files or not data_file:
+      if not extracted_files or not data_file_name: #nenhum arquivo foi extraido ou nenhum arquivo de dados foi encontrado
          raise RuntimeError("Extração do arquivo zip num diretório temporário falhou")
       
-      return os.path.join(self.EXTRACTED_FILES_DIR, data_file) #retorna o caminho para o arquivo extraido
+      return os.path.join(self.EXTRACTED_FILES_DIR, data_file_name) #retorna o caminho para o arquivo extraido
 
    def __dataframe_from_link(self, url:str , data_type: BaseDataType)->pd.DataFrame:
-      file_path: str = self.__download_extract_zipfile(url)
+      file_path: str = self.__download_and_extract_zipfile(url)
 
       df:pd.DataFrame
       if data_type == BaseDataType.EXCEL:
          df:pd.DataFrame = pd.read_excel(file_path)
+
+      """
+      TODO
+      Colocar os casos para os outros tipos de arquivos, caso seja possível extrair dataframes deles
+      """
 
       if df is None:
          raise RuntimeError("não foi possível criar um dataframe a partir do link")
