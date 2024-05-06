ace48ca04e03f871cdfb587ae28c245d735ea440
caue <cauepaivalira@outlook.com>
2024-05-05 22:27:22 -0300

    classe e funções para extrair os dados especificos do dataframe

---
+++ scrapping_caracte_socioeco/DataPointsInfo.py
@@ -0,0 +1,71 @@
+from enum import Enum
+from typing import Any
+
+class DataPointTypes(Enum):
+   INT = int
+   FLOAT = float
+   STRING = str
+   BOOL = bool
+
+class DataPoint():
+   data_name: str
+   column_name:str
+   data_type: DataPointTypes
+   multiply_amount: int 
+
+   def __init__(
+      self,
+      data_name:str,
+      column_name:str,
+      data_type:DataPointTypes, 
+      multiply_amount: int | None = None
+   )-> None:
+      
+      self.data_name:str =  data_name
+      self.column_name:str = column_name
+      self.data_type: DataPointTypes = data_type
+      multiply_amount: int | float | None = multiply_amount
+     
+      if multiply_amount is None:
+         self.multiply_amount = 1
+      else:
+         if self.data_type not in [DataPointTypes.INT, DataPointTypes.FLOAT]:
+            raise IOError("Valor de multiplicação não é valido para tipos diferentes de inteiros ou float")
+         self.multiply_amount = multiply_amount
+
+   def multiply_value(self, value:Any)-> int | float:
+      if self.data_type not in [DataPointTypes.INT, DataPointTypes.FLOAT]:
+         return value
+      else:
+         return value * self.multiply_amount
+      
+class DataPointsInfo():
+   year_column_name: str
+   city_column_name:str
+   data_point_list: list[DataPoint]
+
+   def __init__(self, year_column_name:str, city_column_name:str) -> None:
+      self.year_column_name = year_column_name
+      self.city_column_name = city_column_name
+      self.data_point_list = []
+
+   def add_data_point(
+      self,
+      data_name:str,
+      column_name:str,
+      data_type:DataPointTypes,
+      multiply_amount: int | None = None
+   )->None:
+       self.data_point_list.append(DataPoint(data_name,column_name,data_type,multiply_amount))
+
+   def add_data_point_dict(self,data_dict:dict[str, str|int])->None:
+      data_name:str = data_dict["data_name"]
+      column_name:str = data_dict["column_name"]
+      data_type:DataPointTypes = data_dict["data_type"]
+      multiply_amount: int | None = data_dict.get("multiply_amount")
+
+      self.data_point_list.append(DataPoint(data_name,column_name,data_type,multiply_amount))
+
+   def add_data_points_dicts(self, dict_list: list[dict[str, str|int]])->None:
+      for data_point in dict_list:
+          self.add_data_point_dict(data_point)
--- scrapping_caracte_socioeco/IbgeBasesDataExtractor.py
+++ scrapping_caracte_socioeco/IbgeBasesDataExtractor.py
@@ -1,29 +1,55 @@
 import pandas as pd
 import os
-from AbstractDataExtractor import DataFrameInfo, DataPoint, DataPointTypes
+from DataPointsInfo import DataPointsInfo, DataPointTypes
 
 CITY_COLUMN = "municipio"
 YEAR_COLUMN = "ano"
 
-def extract_data_points(df:pd.DataFrame, data_info: DataFrameInfo)->pd.DataFrame:
-   new_df:pd.DataFrame = pd.DataFrame()
+"""
+Existem cidades com nomes duplicados, vamos ter que usar uma nova chave para identificar elas, provavelmente o id de muncípios do ibge
+talvez popular a tabela de município com isso como primary key
 
-   new_df[CITY_COLUMN] = df[data_info.city_column_name].copy()
-   new_df[YEAR_COLUMN] = df[data_info.year_column_name].copy()
+"""
 
-   for point in data_info.data_point_list:
-      new_df[point.stardard_name] = df[point.column_name].apply(point.multiply_value)
 
-   return new_df
 
-class IbgeBasesDataExtractor():
-   def __init__(self) -> None:
-      pass
+class CategoryDataExtractor():
+
+   category:str
+
+   def __init__(self, category:str) -> None:
+      self.category = category
+
+   def extract_data_points(self, df:pd.DataFrame, data_info: DataPointsInfo)->pd.DataFrame:
+      new_df:pd.DataFrame = pd.DataFrame()
+
+      new_df[CITY_COLUMN] = df[data_info.city_column_name].copy()
+      new_df[YEAR_COLUMN] = df[data_info.year_column_name].copy()
+      #new_df["dado_nome"] = data_info.
+
+      for point in data_info.data_point_list:
+         new_df[point.data_name] = df[point.column_name].apply(point.multiply_value)
+
+      new_df[YEAR_COLUMN] = new_df[YEAR_COLUMN].astype("category") # a coluna de ano é transformado numa categoria, o que economiza memória
+      return new_df
+
+   def join_category_df(self, df_list:list[pd.DataFrame])->pd.DataFrame:
+      if len(df_list) < 2:
+         raise IOError("Esse função deve ser providas com uma lista de pelo menos 2 Dataframes")
+
+      initial_df: pd.DataFrame = df_list[0]
+
+      for i in range(1,len(df_list)):
+         current_df:pd.DataFrame = df_list[i]
+         initial_df = initial_df.merge(current_df,how="inner",on=["ano","municipio"]) #outer join nas colunas de município e ano
+
+      return initial_df
+
 
 df:pd.DataFrame = pd.read_excel(os.path.join("tempfiles","PIB dos Municípios - base de dados 2010-2021.xlsx"))
 
 pib_percapita = {
-   "standard_name": "PIB per capita",
+   "data_name": "PIB per capita",
    "column_name": """Produto Interno Bruto per capita, 
 a preços correntes
 (R$ 1,00)""",
@@ -32,7 +58,7 @@ a preços correntes
 }
 
 pib_agro = {
-   "standard_name": "PIB Agropecuária",
+   "data_name": "PIB Agropecuária",
    "column_name": """Valor adicionado bruto da Agropecuária, 
 a preços correntes
 (R$ 1.000)""",
@@ -40,12 +66,29 @@ a preços correntes
    "multiply_amount": 1000
 }
 
-data_point_list = [DataPoint(pib_percapita),DataPoint(pib_agro)]
-df_info = DataFrameInfo("Ano","Nome do Município",data_point_list)
 
-df2 = extract_data_points(df,df_info)
+df_info1 = DataPointsInfo("Ano","Nome do Município")
+df_info1.add_data_points_dicts([pib_agro])
 
-print(df2.head(5))
+df_info2 = DataPointsInfo("Ano","Nome do Município")
+df_info2.add_data_points_dicts([pib_percapita])
+
+extractor = CategoryDataExtractor("teste")
+
+df1 = extractor.extract_data_points(df,df_info1)
+df2 = extractor.extract_data_points(df,df_info2)
+duplicates_1 = df1[df1.duplicated(subset=['ano', 'municipio'], keep=False)]
+duplicates_2 = df2[df2.duplicated(subset=['ano', 'municipio'], keep=False)]
+
+print(f"Duplicate rows in initial_df:\n{duplicates_1}")
+print(f"Duplicate rows in current_df:\n{duplicates_2}")
+
+
+df3 = extractor.join_category_df([df1,df2])
+
+#print(df3.head(5))
+#print(df3.info())
+
 
 
 
