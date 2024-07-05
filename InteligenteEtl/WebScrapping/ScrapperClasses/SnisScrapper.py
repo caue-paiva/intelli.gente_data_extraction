cacaccca7a353fefd1fd2ed5e7bf715dc6163171
caue <cauepaivalira@outlook.com>
2024-07-05 00:10:48 -0300

    FEAT: scrapper do snis basicamente pronta, falta o data extractor

--- InteligenteEtl/WebScrapping/ScrapperClasses/SnisScrapper.py
+++ InteligenteEtl/WebScrapping/ScrapperClasses/SnisScrapper.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
 import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -9,15 +8,12 @@ from selenium.webdriver.remote.webelement import WebElement
 # Specify the path to the chromedriver executable
 
 # Initialize the Chrome driver
-driver = webdriver.Chrome()
-YEARS_EXTRACTED_PER_RUN = 3
-MAX_YEARS = 3
+
+YEARS_EXTRACTED_PER_RUN = 1
+MAX_YEARS = 2
 INDICATOR_CATEGORIES = ["AE - Informações gerais","AE - Indicadores operacionais - água","AE - Indicadores operacionais - esgotos",
 "RS - Informações sobre coleta domiciliar e pública","RS - Informações sobre coleta seletiva e triagem"]
 
-url = "http://app4.mdr.gov.br/serieHistorica/municipio/index/"
-driver.get(url)
-driver.maximize_window()
 
 def close_select_window(element)->bool:
    try:
@@ -43,7 +39,6 @@ def wait_for_progress_bar(driver:webdriver.Chrome, loading_data_table:bool = Fal
       WebDriverWait(driver, 10).until(
          EC.presence_of_element_located((By.CLASS_NAME, "progress-striped"))
       )
-      # Wait for the progress bar to disappear
       WebDriverWait(driver, 20).until(
          EC.invisibility_of_element_located((By.CLASS_NAME, "progress-striped"))
       )
@@ -62,13 +57,10 @@ def get_csv_link(driver:webdriver.Chrome)->str:
    try:
       time.sleep(6)
       generate_table_button: WebElement = driver.find_element(By.ID,"bt_relatorio")
-      print(generate_table_button.text)
       generate_table_button.click()
 
       time.sleep(10)
-      
-      print("esperando carregar link")
-
+   
       class HrefContainsCsv: #classe para usar no selenium, serve para verificar e esperar caso o link do CSV n apareça
         def __init__(self, locator):
             self.locator = locator
@@ -85,7 +77,6 @@ def get_csv_link(driver:webdriver.Chrome)->str:
         HrefContainsCsv((By.CLASS_NAME, "btn-af"))
       )
 
-    # Print the href link
       href_link = a_tag.get_attribute('href')
       print("The href link is:", href_link)
       return href_link
@@ -107,11 +98,9 @@ def select_necessary_indicators(element:WebElement)->bool:
    try: 
       options_list = element.find_elements(By.TAG_NAME,"input")
       parsed_categories:list[str] = list(map(lambda x: x.lower().replace(" ","") ,INDICATOR_CATEGORIES))
-      print(parsed_categories)
       for option in options_list:
          option_name:str = option.get_attribute("title")
          parsed_option:str = option_name.lower().replace(" ","")  
-         print(parsed_option)
          if parsed_option in parsed_categories:
             option.click()
       return True
@@ -119,10 +108,9 @@ def select_necessary_indicators(element:WebElement)->bool:
       print(f"Falha ao selecionar os indicadores necessários: {e}")
       return False
 
-def extract_snis(driver:webdriver.Chrome, years_to_extract_in_run:list[int],first_run:bool =False):
-
-   try:
-      
+def __extraction_run(driver:webdriver.Chrome, all_years:list[int] ,first_run:bool =False)->tuple[str,list[int]]:   
+   csv_link:str = ""
+   try:   
       #clica na aba de dados dos munícipios agrupados
       link_element = driver.find_element(By.XPATH, "//a[@link='/serieHistorica/consolidadoMunicipio/index']")
       time.sleep(2)
@@ -146,32 +134,26 @@ def extract_snis(driver:webdriver.Chrome, years_to_extract_in_run:list[int],firs
       if first_run: #primeira rodada da extração
          for input_element in input_elements:
             year:str = input_element.get_attribute('value')
-            years_to_extract_in_run.append(int(year))
-
-            if len(years_to_extract_in_run) >= MAX_YEARS:
+            all_years.append(int(year))
+            if len(all_years) >= MAX_YEARS:
                break
-     
-      next_list_years = years_to_extract_in_run[YEARS_EXTRACTED_PER_RUN:] #lista de anos excluindo os já extraidos
-      years_to_extract_in_run = years_to_extract_in_run[:YEARS_EXTRACTED_PER_RUN] #anos para serem extraidos nessa rodada
+
+      years_to_extract_in_run = all_years[:YEARS_EXTRACTED_PER_RUN] #anos para serem extraidos nessa rodada
       
-      select_years(input_elements,years_to_extract_in_run)
-      close_select_window(select_years_div) 
+      select_years(input_elements,years_to_extract_in_run) #seleciona o anos que vão ser extraidos
+      close_select_window(select_years_div)  #fecha a janela de opções de ano
 
       print("List of years cur:", years_to_extract_in_run)
-      print("List of years next:", next_list_years)
+      print("List of years next:", all_years[YEARS_EXTRACTED_PER_RUN:])
 
       fieldsets = driver.find_elements(By.CLASS_NAME, "grupo")
       for fieldset in fieldsets:
          legend = fieldset.find_element(By.TAG_NAME, "legend")
-         
          if "município" in legend.text.lower(): #clica no 
-               print(legend.text)
-               # Find the button inside the fieldset and click it
                button = fieldset.find_element(By.TAG_NAME, "button")
                button.click()
-               # Wait for the progress bar to appear
                wait_for_progress_bar(driver)
-               break  # Exit the loop after finding and clicking the button
+               break
    
       WebDriverWait(driver, 10).until(
          EC.presence_of_element_located((By.ID, "multiselect_menu_cod_mun"))
@@ -192,18 +174,16 @@ def extract_snis(driver:webdriver.Chrome, years_to_extract_in_run:list[int],firs
       
       click_query_button(driver) #clica botão de pesquisa, vai pra segunda parte da busca
 
+      time.sleep(2)
       WebDriverWait(driver, 10).until(
          EC.presence_of_element_located((By.ID, "div_informacao"))
       )
-
+      #acha o elemento <p> com o botão para clicar que mostra as opções de indicadores
       p_element = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, "//p[label[@for='cod_fam_info']]"))
       )
 
-      # Print the text of the <p> element
-      print(p_element.text)
-
-      # Find the button inside the div and click it
+      #clica no botão para mostra o menu de selecionar os indicadores
       button = p_element.find_element(By.TAG_NAME, "button")
       button.click()
 
@@ -211,26 +191,39 @@ def extract_snis(driver:webdriver.Chrome, years_to_extract_in_run:list[int],firs
          EC.presence_of_element_located((By.ID, "multiselect_menu_cod_fam_info"))
       )
 
-      time.sleep(4)
-      select_necessary_indicators(indacator_options_div)
-      time.sleep(1)
-
-
-
-      # Find the <a> element that contains a <span> with the text "Marcar todos"
-     # marcar_todos_a = multiselect_div.find_element(By.XPATH, ".//a[.//span[text()='Marcar todos']]")
-     # marcar_todos_a.click()
+      time.sleep(3)
+      select_necessary_indicators(indacator_options_div) #seleciona os indicadores necessários
       
       time.sleep(2)
-      close_select_window(indacator_options_div)
+      close_select_window(indacator_options_div) #fecha janela de seleção das opções
       time.sleep(2)
       click_query_button(driver)
       wait_for_progress_bar(driver,True)
       time.sleep(2)
-      get_csv_link(driver)
+      csv_link = get_csv_link(driver)
+      return csv_link,years_to_extract_in_run #retorna o link do CSV e a lista de anos a que ele se refere
    
-   finally:
-      # Close the driver
-      driver.quit()
+   except Exception as e:
+      print(f"Falha ao tentar extrair os anos {years_to_extract_in_run}: {e}")
+      return "",[]
 
-extract_snis(driver,[],True)
+def extract_snis()->bool:
+   url = "http://app4.mdr.gov.br/serieHistorica/municipio/index/"
+   driver = webdriver.Chrome()
+   driver.get(url)
+   driver.maximize_window()
+   years_to_extract_list:list[int] = []
+   list_of_csvs_and_their_years:list[tuple[str,list[int]]] = []
+
+   __extraction_run(driver,years_to_extract_list,True) #primeira rodada do algoritmo, pega a lista de todos os anos
+   print(years_to_extract_list)
+   
+   years_to_extract_list = years_to_extract_list[YEARS_EXTRACTED_PER_RUN:]
+   while years_to_extract_list:
+      driver.refresh()
+      time.sleep(3)
+      run_result:tuple[str,list[int]] = __extraction_run(driver,years_to_extract_list,False)
+      list_of_csvs_and_their_years.append(run_result)
+      years_to_extract_list = years_to_extract_list[YEARS_EXTRACTED_PER_RUN:]
+
+extract_snis()
