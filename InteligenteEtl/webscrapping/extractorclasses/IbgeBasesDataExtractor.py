a93f775103aecc5440493e3897d37bc70a028874
“Caue <cauepaivalira@outlook.com>
2024-07-28 21:40:09 -0300

    CHANGE1: Classe IbgeBasesDataExtractor agora  remove linhas com valores NULL do df

--- InteligenteEtl/webscrapping/extractorclasses/IbgeBasesDataExtractor.py
+++ InteligenteEtl/webscrapping/extractorclasses/IbgeBasesDataExtractor.py
@@ -1,10 +1,7 @@
 import pandas as pd
-import os
-from webscrapping.extractorclasses import TableDataPoints
 from webscrapping.extractorclasses import AbstractDataExtractor
 from webscrapping.scrapperclasses import IbgePibCidadesScrapper
-from typing import Type
-from datastructures import DataTypes, BaseFileType, ProcessedDataCollection, YearDataPoint
+from datastructures import DataTypes, ProcessedDataCollection, YearDataPoint
 from enum import Enum
 
 """
@@ -35,17 +32,16 @@ class CitiesGDPDataInfo(Enum):
       "dtype": DataTypes.FLOAT,
       "multiply_amount": 1000
    }
+   INDUSTRY_GDP = {
 
-
-
-
+   }
 
 class CategoryDataExtractor(AbstractDataExtractor):
 
    EXTRACTED_DATA_YEAR_COL = "Ano"
    EXTRACTED_DATA_CITY_CODE_COL = "Código do Município"
 
-   def extract_processed_collection(self,scrapper: Type[IbgePibCidadesScrapper], data_info: TableDataPoints)->list[ProcessedDataCollection]:
+   def extract_processed_collection(self,scrapper: IbgePibCidadesScrapper)->list[ProcessedDataCollection]:
       """ 
       TODO: Completar a função 
 
@@ -53,26 +49,11 @@ class CategoryDataExtractor(AbstractDataExtractor):
       extrai o dataframe bruto da base em um df menor apenas com as colunas de ano, código de cidade e os pontos de dados buscados
       
       Args:
-        scrapper (Type[IbgePibCidadesScrapper]) : um objeto dessa classe ou de uma classe filha que implemente a interface extract_base 
-         data_info (TableDataPoints) : Classe para especificar onde e como os dados buscados estão no df bruto
-      
+        scrapper (IbgePibCidadesScrapper) : um objeto dessa classe ou de uma classe filha que implemente a interface extract_base       
       Return:
-         (dataframe): dataframe com as colunas: ano, código_muni, dado_identificador, valor e tipo_dado. Esse formato será similar aos 
-         dados em cada tabela de categoria
+        (list[ProcessedDataCollection]): lista de objetos ProcessedDataCollection, cada um sendo um dado diferente e pronto para entrar no BD
       """
-      
-      raw_df:pd.DataFrame = scrapper.extract_database()
-      processed_df = self.extract_data_points(df,data_info)
 
-      data_collection_list: list[ProcessedDataCollection] = []
-
-      for data_point in data_info.data_column_list:
-            collection = ProcessedDataCollection(
-               data_point.data_category,
-               data_point.data_name,
-            )
-
-   def extract_data(self,scrapper: IbgePibCidadesScrapper):
       list_datapoints:list[YearDataPoint] = scrapper.extract_database()
       parsed_datapoints:list[YearDataPoint] = self.__drop_cols(list_datapoints)
       
@@ -89,63 +70,6 @@ class CategoryDataExtractor(AbstractDataExtractor):
          )
       
       return processed_data_list
-
-   def extract_data_points(self, df: pd.DataFrame, data_info: TableDataPoints)->pd.DataFrame:
-      """ 
-      Extrai o dataframe bruto da base em um df menor apenas com as colunas de ano, código de cidade e os pontos de dados buscados
-      
-      Args:
-         df (dataframe) : dataframe bruto da base de dados
-         data_info (TableDataPoints) : Classe para especificar onde e como os dados buscados estão no df bruto
-      
-      Return:
-         (dataframe): dataframe com as colunas: ano, código_muni, dado_identificador, valor e tipo_dado. Esse formato será similar aos 
-         dados em cada tabela de categoria
-      """
-
-      if len(data_info.data_column_list) < 1:
-         raise IOError("Lista de ponto de dados deve conter pelo menos 1 ponto de dado")
-      
-      df = self.update_city_code(df,data_info.city_code_column) #checa se o código dos municípios está correto
-      df.columns = self.__parse_df_col_names(df.columns)
-
-      final_df:pd.DataFrame = pd.DataFrame() #df final a ser retornado
-      city_code_col:str = self.parse_strings(data_info.city_code_column) #parsing nos nomes para removes espaços e colocar em lowercase 
-      year_col:str =  self.parse_strings(data_info.year_column_name)
-      
-      for point in data_info.data_column_list: #loop pela lista de colunas com os dados
-         temp_df:pd.DataFrame = pd.DataFrame() #cria df temporário
-         data_val_col:str =  self.parse_strings(point.column_name)
-
-         temp_df[self.CITY_CODE_COL] = df[city_code_col].copy() #copia as colunas de codigo de cidade e ano
-         temp_df[self.YEAR_COLUMN] = df[year_col].copy()
-         temp_df[self.DATA_IDENTIFIER_COLUMN] = point.data_name #coluna de identificador do dado recebe o nome do dado
-         temp_df[self.DATA_VALUE_COLUMN] = df[data_val_col].apply(point.multiply_value) #preenche a coluna de valor de dados
-         temp_df[self.DTYPE_COLUMN] = point.data_type.value #coluna de tipo de dados
-
-         final_df = pd.concat(objs=[final_df,temp_df], axis='index', ignore_index=True) #concatena as linhas do df temp no df total
-
-      final_df[self.YEAR_COLUMN] = final_df[self.YEAR_COLUMN].astype("category") # a coluna de ano e de tipo de dado é transformado numa categoria, o que economiza memória
-      final_df[self.DTYPE_COLUMN] = final_df[self.DTYPE_COLUMN].astype("category")
-      final_df[self.DATA_IDENTIFIER_COLUMN] = final_df[self.DATA_IDENTIFIER_COLUMN].astype("category")
-      
-      return final_df
-
-   def __parse_df_col_names(self,columns: list[str])->list[str]:
-      """
-      Recebe uma lista com o nome das colunas do df e retorna essa lista com os nomes processados.
-      O processamento se dá por remover os espaços em branco e \ n, já que algumas bases tem nomes de colunas complexos e 
-      longos que os tornam difíceis de acessar.
-
-      Args:
-         columns (list[int]): lista de strings que representam a lista de colunas do dataframe
-
-      Return:
-         (list[int]): lista de strings processadas 
-      
-      """
-      new_cols = list(map(self.parse_strings,columns))
-      return new_cols
 
    def __change_col_dtypes(self,df:pd.DataFrame)->pd.DataFrame:
       df[self.CITY_CODE_COL] = df[self.CITY_CODE_COL].astype("int")
@@ -190,8 +114,7 @@ class CategoryDataExtractor(AbstractDataExtractor):
             axis="columns"
          ) #muda o nome da coluna de código dos municípios e do ano dos dados
 
-
-
+         new_df = new_df.dropna(axis="index") #tira as linhas com valores nulos
          parsed_data_points.append(
             YearDataPoint(new_df,datapoint.data_year)
          )   
@@ -243,22 +166,6 @@ if __name__ == "__main__":
       "data_type": DataTypes.FLOAT,
       "multiply_amount": 1000
    }
-   
-   df:pd.DataFrame = pd.read_excel(os.path.join("webscrapping","tempfiles","PIB dos Municípios - base de dados 2010-2021.xlsx"))
-   df_info1 = TableDataPoints("Ano","Código do Município")
-   df_info1.add_data_points_dicts([pib_agro])
-
-  # df_info2 = TableDataPoints("Ano","Código do Município")
-   df_info1.add_data_points_dicts([pib_percapita])
-
-   url2 =  "https://www.ibge.gov.br/estatisticas/economicas/contas-nacionais/9088-produto-interno-bruto-dos-municipios.html?=&t=downloads"
-
-   scrapper = IbgePibCidadesScrapper(url2,BaseFileType.EXCEL,True)
-   extractor = CategoryDataExtractor()
-
-   df1 = extractor.extract_data_points(scrapper,df_info1)
-   print(df1.head(5))
-   df1.to_csv("teste.csv")
    
 
 
